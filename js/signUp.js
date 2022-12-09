59a1fa8aefb1cc3f284ae472ec3df67d53c0d184
mosidrum <mosiokanga@gmail.com>
2022-12-09 05:21:49 +0100

    done with signing up, I will validate it later

--- js/signUp.js
+++ js/signUp.js
@@ -1,5 +1,7 @@
 function signUpScreen (){
-    const signUp = document.querySelector('.sign-up');
+    const signUp = document.querySelector('sign-up');
     signUp.classList.toggle('active');
+    const popup = document.querySelector('signUpForm');
+    popup.classList.toggle('active');
 }
 
