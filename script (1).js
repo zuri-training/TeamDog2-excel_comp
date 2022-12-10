c22b841abdc0e71e386a1e1795d1873bf1ed4ee5
Nnanwuno chidimma <chidimmannanwuno@gmail.com>
2022-12-10 21:45:27 +0100

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,9 @@
+function toggleNav() {
+   var x = document.getElementsByTagName("nav")[0];
+   if (x.className === "bottomNav") {
+      x.className += " openNav";
+   } else {
+      x.className = "bottomNav";
+   }
+}
+document.querySelector(".hamburger").addEventListener("click", toggleNav);
